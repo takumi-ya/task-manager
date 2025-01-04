7c80d4f5753808c53b666c7b22f79e95f09642a7
Takumi Yamada <110591229+takumi-ya@users.noreply.github.com>
2025-01-04 23:00:33 +0900

    taskモデルの構造を変更

--- internal/models/task.go
+++ internal/models/task.go
@@ -9,8 +9,11 @@ import (
 type Task struct {
 	bun.BaseModel `bun:"table:tasks,alias:t"`
 
-	ID    int64     `bun:"id,pk,autoincrement"`
-	Name  string    `bun:"name,notnull"`
-	Done  bool      `bun:"done"`
-	Until time.Time `bun:"until"`
+	ID       int64     `bun:"id,pk,autoincrement"`
+	Name     string    `bun:"name,notnull"`
+	Done     bool      `bun:"done"`
+	Until    time.Time `bun:"until"`
+	CreateAt time.Time `bun:",nullzero,notnull,default:current_timestamp"`
+	UserID   int64     `bun:"user_id"`
+	User     *User     `bun:"rel:belongs-to,join:user_id=id"`
 }
